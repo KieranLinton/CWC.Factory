8892204c149e49da9b93d6d606a7c1f2a1b9a6a2
soggyDoughnut <proton5@live.com>
2019-02-22 14:42:33 +0100

    ok added the two classes :)

---
+++ CWC.Domain.Objects/Person/PersonClaim.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CWC.Domain.Objects.Person
+{
+   
+class PersonClaim
+    {
+        public string id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string dateJoined { get; set; }
+        public bool isActive { get; set; }
+
+
+    }
+}
---
+++ CWC.Services/Person/PersonService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWC.Services.Person
+{
+    class PersonService
+    {
+        public void AddPerson() {
+            
+        }
+        public void UpdatePerson()
+        {
+
+        }
+        public void QueryPerson()
+        {
+
+        }
+        public void DeletePerson()
+        {
+
+        }
+    }
+}
